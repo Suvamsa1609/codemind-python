d2e0618c39020e85b2ce8d400bc2dc0dab0a7526
Suvamsa1609 <96298524+Suvamsa1609@users.noreply.github.com>
2022-11-30 22:52:52 +0530

    n=int(input()) d=list(map(int,input().split())) a,b=map(int,input().split()) flag=0 min=999 for i in range(n):     if d[i]>=a and d[i]<=b:         if d[i]<min:             min=d[i]             flag=1 if flag==0:     print("-1") else:     print(min)

---
+++ minimum_between_A_and_B.py
@@ -0,0 +1,14 @@
+n=int(input())
+d=list(map(int,input().split()))
+a,b=map(int,input().split())
+flag=0
+min=999
+for i in range(n):
+    if d[i]>=a and d[i]<=b:
+        if d[i]<min:
+            min=d[i]
+            flag=1
+if flag==0:
+    print("-1")
+else:
+    print(min)
