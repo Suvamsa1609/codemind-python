da53d11148793450de47dc9a6bd8aa28425be5bb
Suvamsa1609 <96298524+Suvamsa1609@users.noreply.github.com>
2022-12-18 18:09:24 +0530

    def fun(n):     for i in range(n,0,-1):         print(i,end=" ")         i-=1 n=int(input()) for i in range(n):     fun(n)     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,5 +1,8 @@
+def fun(n):
+    for i in range(n,0,-1):
+        print(i,end=" ")
+        i-=1
 n=int(input())
-for i in range(1,n+1):
-    for j in range(1,i+1):
-        print(j, end = '')
+for i in range(n):
+    fun(n)
     print()
