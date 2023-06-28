e5a3cd21a24988d90458bba37c05dd76baa07f8c
Suvamsa1609 <96298524+Suvamsa1609@users.noreply.github.com>
2023-06-28 09:42:54 +0530

    def pal(s):     if s == s[::-1]:         return True     else:         return False s = input().lower() s = s.split(' ') c = 0 for i in s:     if pal(i) == True:         c+=1 print(c)

---
+++ Count_Palindrome_words_in_a_string.py
@@ -0,0 +1,12 @@
+def pal(s):
+    if s == s[::-1]:
+        return True
+    else:
+        return False
+s = input().lower()
+s = s.split(' ')
+c = 0
+for i in s:
+    if pal(i) == True:
+        c+=1
+print(c)
